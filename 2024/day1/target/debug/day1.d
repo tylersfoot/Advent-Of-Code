bc7bf7044a2006c252176597a3ab6ecbb2d8bd47
tylersfoot <tylerdepa8@gmail.com>
2024-12-25 15:41:42 -0500

    fix pt1

--- 2024/day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-C:\Users\tyler\Desktop\GitHub\Advent-Of-Code\2024\day1\target\debug\day1.exe: C:\Users\tyler\Desktop\GitHub\Advent-Of-Code\2024\day1\src\main.rs
--- 2024/day1/target/debug/deps/day1.d
+++
@@ -1,5 +0,0 @@
-C:\Users\tyler\Desktop\GitHub\Advent-Of-Code\2024\day1\target\debug\deps\day1.exe: src/main.rs
-
-C:\Users\tyler\Desktop\GitHub\Advent-Of-Code\2024\day1\target\debug\deps\day1.d: src/main.rs
-
-src/main.rs:
